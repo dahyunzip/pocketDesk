a53f4c0d711c0ddbd186c86d5dc274915c9adef8
Kim Dahyun <dhkim12@i-gns.co.kr>
2026-05-29 16:18:30 +0900

    fix:pg 명시적 로드 및 타입 추가

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize'
+import pg from 'pg'
 
 const databaseUrl = process.env.DATABASE_URL || 'postgresql://localhost:5433/pocketdesk'
 
 export const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
+  dialectModule: pg,
   logging: false,
   define: {
     timestamps: true,
